184530fc6a0f9f048a843b316e295df4f5e14c7d
J-Eugenio <eugeniopb4@gmail.com>
2020-07-09 20:12:40 -0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import {AppRegistry} from 'react-native'
+import Navigator from './src/Navigator'
+import {name as appName} from './app.json'
+import {YellowBox} from 'react-native';
+import storeConfig from './src/store/storeConfig'
+
+import axios from 'axios'
+axios.defaults.baseURL = 'https://lambe-fde43.firebaseio.com/'
+
+YellowBox.ignoreWarnings(['Warning: ...']);
+console.disableYellowBox = true;
+
+const store = storeConfig()
+const Redux = () => (
+    <Provider store={store}>
+        <Navigator />
+    </Provider>
+)
+
+AppRegistry.registerComponent(appName, () => Redux)
