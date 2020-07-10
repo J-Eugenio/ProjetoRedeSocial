85d4847e32a3d27d7ac7892e3e4cd0ac30e302da
J-Eugenio <eugeniopb4@gmail.com>
2020-07-09 22:42:02 -0300

    Salvando Imagem no firebase

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,42 @@
 const functions = require('firebase-functions');
+const cors = require('cors')({ origin: true })
+const fs = require('fs')
+const uuid = require('uuid-v4')
+const { Storage } = require('@google-cloud/storage')
+const storage = new Storage({
+    projectId: 'lambe-fde43',
+    keyFilename: 'lambe-fde43.json'
+})
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
+exports.uploadImage = functions.https.onRequest((request, response) => {
+    cors(request, response, () => {
+        try {
+            fs.writeFileSync('/tmp/imageToSave.jpg',
+                request.body.image, 'base64')//salvando a imagem local descriptografada
+            const bucket = storage.bucket('lambe-fde43.appspot.com')//url base do storage do firebase
+            const id = uuid()//gera um id
+            bucket.upload('/tmp/imageToSave.jpg', {
+                uploadType: 'media',//tipo de arquivo enviado
+                destination: `/posts/${id}.jpg`,//template string do destino
+                metadata: {
+                    metadata: {
+                        contentType: 'image/jpeg',
+                        firebaseStorageDownloadTokens: id
+                    }
+                }
+            }, (err, file) => {
+                if (err) {
+                    return response.status(500).json({ error: err })
+                } else {
+                    const fileName = encodeURIComponent(file.name)
+                    const imageUrl = 'https://firebasestorage.googleapis.com/v0/b/'
+                        + bucket.name + '/o/' + fileName + '?alt=media&token=' + id
+                    return response.status(201).json({ imageUrl: imageUrl }) //retorna a imageUrl caso de tudo certo
+                }
+            })
+        } catch{
+            console.log(err)
+            return response.status(500).json({ error: err })
+        }
+    })
 });
--- src/store/actions/posts.js
+++ src/store/actions/posts.js
@@ -1,11 +1,25 @@
 import { ADD_POST, ADD_COMMENT } from './actionTypes'
 import axios from 'axios'
 
-export const addPost = post =>{
+export const addPost = post => {
     return dispatch => {
-        axios.post('/posts.json', {...post })
-            .catch(err => console.log(err))
-            .then(res => console.log(res.data))
+        axios({//primeiro verifica se a imagem foi enviada
+            url: 'uploadImage',
+            baseURL: 'https://us-central1-lambe-fde43.cloudfunctions.net',
+            method: 'post',
+            data: {
+                image: post.image.base64
+            }
+        })
+            .catch(err => console.log(err.error))
+            .then(resp => {
+
+                post.image = resp.data.imageUrl//copia a url da imagem para o state local image
+                axios.post('/posts.json', { ...post })//persiste os dados no banco
+                    .catch(err => console.log(err))
+                    .then(res => console.log(res.data))
+            })
+
     }
 
     /*return {
