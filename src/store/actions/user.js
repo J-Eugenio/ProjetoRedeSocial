14dd1502eba2f22bcf3816811d5fe1d44be1f3b3
J-Eugenio <eugeniopb4@gmail.com>
2020-07-15 20:55:13 -0300

    Login do usuárop

--- src/store/actions/user.js
+++ src/store/actions/user.js
@@ -1,9 +1,14 @@
-import { USER_LOGGED_IN ,USER_LOGGED_OUT } from './actionTypes'
+import {
+    USER_LOGGED_IN,
+    USER_LOGGED_OUT,
+    LOADING_USER,
+    USER_LOADED
+} from './actionTypes'
 import axios from 'axios'
 import { API_KEY } from './KEY_API'
 const authBaseURL = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty'
 
-export const login = user => {
+export const userLogged = user => {
     return {
         type: USER_LOGGED_IN,
         payload: user
@@ -11,14 +16,49 @@ export const login = user => {
 }
 
 export const logout = () => {
-    return{
+    return {
         type: USER_LOGGED_OUT
     }
 }
 
-export const createUser = user =>{
+export const createUser = user => {
     return dispatch => {
-        axios.post(`${authBaseURL}/signupNewUser?key=${API_KEY}`,{
+        axios.post(`${authBaseURL}/signupNewUser?key=${API_KEY}`, {
+            email: user.email,
+            password: user.password,
+            returnSecureToken: true
+        })
+            .catch(err => console.log(err))
+            .then(res => {
+                if (res.data.localId) {
+                    axios.put(`/users/${res.data.localId}.json`, {
+                        name: user.name
+                    })
+                        .catch(err => console.log(err))
+                        .then(res => {
+                            console.log('Usuário criado com sucesso')
+                        })
+                }
+            })
+    }
+}
+
+export const loadingUser = () => {
+    return {
+        type: LOADING_USER
+    }
+}
+
+export const userLoaded = () => {
+    return {
+        type: USER_LOADED
+    }
+}
+
+export const login = user => {
+    return dispatch => {
+        dispatch(loadingUser())
+        axios.post(`${authBaseURL}/verifyPassword?key=${API_KEY}`, {
             email: user.email,
             password: user.password,
             returnSecureToken: true
@@ -26,12 +66,13 @@ export const createUser = user =>{
             .catch(err => console.log(err))
             .then(res =>{
                 if(res.data.localId){
-                    axios.put(`/users/${res.data.localId}.json`,{
-                        name: user.name
-                    })
+                    axios.get(`/users/${res.data.localId}.json`)
                         .catch(err => console.log(err))
                         .then(res =>{
-                            console.log('Usuário criado com sucesso')
+                            user.password = null
+                            user.name = res.data.name
+                            dispatch(userLogged(user))
+                            dispatch(userLoaded())
                         })
                 }
             })
